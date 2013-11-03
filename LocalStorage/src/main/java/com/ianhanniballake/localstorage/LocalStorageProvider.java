a2e6167fff1611bd3ea532a1de193e1f4d414194
Ian Lake <ian.hannibal.lake@gmail.com>
2013-11-03 08:34:02 -0800

    Initial commit

---
+++ LocalStorage/src/main/java/com/ianhanniballake/localstorage/LocalStorageProvider.java
@@ -0,0 +1,207 @@
+package com.ianhanniballake.localstorage;
+
+import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.os.CancellationSignal;
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.os.StatFs;
+import android.provider.DocumentsContract.Document;
+import android.provider.DocumentsContract.Root;
+import android.provider.DocumentsProvider;
+import android.util.Log;
+import android.webkit.MimeTypeMap;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class LocalStorageProvider extends DocumentsProvider {
+    private static final String ROOT = Environment.getExternalStorageDirectory().getPath();
+
+    @Override
+    public Cursor queryRoots(String[] projection) throws FileNotFoundException {
+        // Create a cursor with either the requested fields, or the default
+        // projection if "projection" is null.
+        final MatrixCursor result =
+                new MatrixCursor(resolveRootProjection(projection));
+
+        File[] rootDirs = {Environment.getExternalStorageDirectory()};
+
+        // It's possible to have multiple roots (e.g. for multiple accounts in the
+        // same app) -- just add multiple cursor rows.
+        for (File root : rootDirs) {
+            final MatrixCursor.RowBuilder row = result.newRow();
+            row.add(Root.COLUMN_ROOT_ID, root.getAbsolutePath());
+            row.add(Root.COLUMN_SUMMARY, root.getAbsolutePath());
+
+            // FLAG_SUPPORTS_CREATE means at least one directory under the root supports
+            // creating documents. FLAG_SUPPORTS_RECENTS means your application's most
+            // recently used documents will show up in the "Recents" category.
+            // FLAG_SUPPORTS_SEARCH allows users to search all documents the application
+            // shares.
+            row.add(Root.COLUMN_FLAGS, Root.FLAG_LOCAL_ONLY | Root.FLAG_SUPPORTS_CREATE);
+
+            // COLUMN_TITLE is the root title (e.g. Gallery, Drive).
+            row.add(Root.COLUMN_TITLE, getContext().getString(R.string.title));
+
+            // This document id cannot change once it's shared.
+            row.add(Root.COLUMN_DOCUMENT_ID, ROOT);
+
+            row.add(Root.COLUMN_ICON, R.drawable.ic_launcher);
+
+            row.add(Root.COLUMN_AVAILABLE_BYTES, new StatFs(root.getPath()).getAvailableBytes());
+        }
+        return result;
+    }
+
+    @Override
+    public String createDocument(String parentDocumentId, String mimeType, String displayName) throws FileNotFoundException {
+        File newFile = new File(parentDocumentId, displayName);
+        try {
+            newFile.createNewFile();
+            return newFile.getPath();
+        } catch (IOException e) {
+            Log.e(LocalStorageProvider.class.getSimpleName(), "Error creating new file " + newFile);
+        }
+        return null;
+    }
+
+    @Override
+    public AssetFileDescriptor openDocumentThumbnail(String documentId, Point sizeHint, CancellationSignal signal) throws FileNotFoundException {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(documentId, options);
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        options.inSampleSize = 1;
+        if (height > sizeHint.y || width > sizeHint.x) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / options.inSampleSize) > sizeHint.y
+                    && (halfWidth / options.inSampleSize) > sizeHint.x) {
+                options.inSampleSize *= 2;
+            }
+        }
+        options.inJustDecodeBounds = false;
+        Bitmap bitmap = BitmapFactory.decodeFile(documentId, options);
+        File tempFile = null;
+        FileOutputStream out = null;
+        try {
+            tempFile = File.createTempFile("thumbnail", null, getContext().getCacheDir());
+            out = new FileOutputStream(tempFile);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 90, out);
+        } catch (IOException e) {
+            Log.e(LocalStorageProvider.class.getSimpleName(), "Error writing thumbnail", e);
+            return null;
+        } finally {
+            if (out != null)
+                try {
+                    out.close();
+                } catch (IOException e) {
+                    Log.e(LocalStorageProvider.class.getSimpleName(), "Error closing thumbnail", e);
+                }
+        }
+        return new AssetFileDescriptor(ParcelFileDescriptor.open(tempFile, ParcelFileDescriptor.MODE_READ_ONLY), 0, AssetFileDescriptor.UNKNOWN_LENGTH);
+    }
+
+    private String[] resolveRootProjection(String[] projection) {
+        if (projection != null)
+            return projection;
+        else
+            return new String[]{Root.COLUMN_ROOT_ID, Root.COLUMN_SUMMARY, Root.COLUMN_FLAGS, Root.COLUMN_TITLE,
+                    Root.COLUMN_DOCUMENT_ID, Root.COLUMN_ICON, Root.COLUMN_AVAILABLE_BYTES};
+    }
+
+    @Override
+    public Cursor queryChildDocuments(String parentDocumentId, String[] projection, String sortOrder) throws FileNotFoundException {
+        final MatrixCursor result = new
+                MatrixCursor(resolveDocumentProjection(projection));
+        Log.d(LocalStorageProvider.class.getSimpleName(), "Parent Document Id: " + parentDocumentId);
+        final File parent = new File(parentDocumentId);
+        for (File file : parent.listFiles()) {
+            // Adds the file's display name, MIME type, size, and so on.
+            includeFile(result, file);
+        }
+        return result;
+    }
+
+    @Override
+    public Cursor queryDocument(String documentId, String[] projection) throws FileNotFoundException {
+        Log.d(LocalStorageProvider.class.getSimpleName(), "Document Id: " + documentId);
+        // Create a cursor with the requested projection, or the default projection.
+        final MatrixCursor result = new
+                MatrixCursor(resolveDocumentProjection(projection));
+        includeFile(result, new File(documentId));
+        return result;
+    }
+
+    private void includeFile(MatrixCursor result, File file) throws FileNotFoundException {
+        final MatrixCursor.RowBuilder row = result.newRow();
+        row.add(Document.COLUMN_DOCUMENT_ID, file.getPath());
+        row.add(Document.COLUMN_DISPLAY_NAME, file.getName());
+        String mimeType = getDocumentType(file.getPath());
+        row.add(Document.COLUMN_MIME_TYPE, mimeType);
+        int flags = file.canWrite() ? Document.FLAG_SUPPORTS_DELETE | Document.FLAG_SUPPORTS_WRITE : 0;
+        if (mimeType.startsWith("image/"))
+            flags |= Document.FLAG_SUPPORTS_THUMBNAIL;
+        row.add(Document.COLUMN_FLAGS, flags);
+        row.add(Document.COLUMN_SIZE, file.length());
+        row.add(Document.COLUMN_LAST_MODIFIED, file.lastModified());
+    }
+
+    @Override
+    public String getDocumentType(String documentId) throws FileNotFoundException {
+        File file = new File(documentId);
+        if (file.isDirectory())
+            return Document.MIME_TYPE_DIR;
+        final int lastDot = file.getName().lastIndexOf('.');
+        if (lastDot >= 0) {
+            final String extension = file.getName().substring(lastDot + 1);
+            final String mime = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+            if (mime != null) {
+                return mime;
+            }
+        }
+        return "application/octet-stream";
+    }
+
+    @Override
+    public void deleteDocument(String documentId) throws FileNotFoundException {
+        new File(documentId).delete();
+    }
+
+    private String[] resolveDocumentProjection(String[] projection) {
+        if (projection != null)
+            return projection;
+        else
+            return new String[]{Document.COLUMN_DOCUMENT_ID, Document.COLUMN_DISPLAY_NAME, Document.COLUMN_FLAGS,
+                    Document.COLUMN_MIME_TYPE, Document.COLUMN_SIZE, Document.COLUMN_LAST_MODIFIED};
+    }
+
+    @Override
+    public ParcelFileDescriptor openDocument(String documentId, String mode, CancellationSignal signal) throws FileNotFoundException {
+        File file = new File(documentId);
+        final boolean isWrite = (mode.indexOf('w') != -1);
+        if (isWrite) {
+            return ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_WRITE);
+        } else {
+            return ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY);
+        }
+    }
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+}
